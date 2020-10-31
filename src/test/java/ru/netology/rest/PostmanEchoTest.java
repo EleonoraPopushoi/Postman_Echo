f2fdd8f4f0684355c8b64f393e140fdeda163c8d
Eleonora Popushoi <eleonorapopushoi@gmail.com>
2020-10-31 12:37:22 +0200

    passed test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
     @Test
@@ -16,7 +17,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("postman.schema.json"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
